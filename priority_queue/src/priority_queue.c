b3f6ef1978904c510978f1dafe7bbfb87f31488b
Luhrel <57545107+Luhrel@users.noreply.github.com>
2023-05-11 08:26:33 +0200

    Working SIMD version

--- priority_queue/src/priority_queue.c
+++ priority_queue/src/priority_queue.c
@@ -2,26 +2,27 @@
 #include <assert.h>
 #include <emmintrin.h>
 #include <immintrin.h>
+#include <smmintrin.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
 #include <tmmintrin.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG
 
 void print128(char* label, __m128i var) {
   int32_t val[4];
   memcpy(val, &var, sizeof(val));
-  printf("%s: %x %x %x %x \n", label, val[0], val[1], val[2], val[3]);
+  printf("%s:\t %x %x %x %x \n", label, val[0], val[1], val[2], val[3]);
 }
 
 void print256(char* label, __m256i var) {
   node_t* val[4];
   memcpy(val, &var, sizeof(val));
 
-  printf("%s : ", label);
+  printf("%s:\t ", label);
   for (int i = 0; i < 4; ++i) {
     printf("%x ", val[i]->fCost);
   }
@@ -30,14 +31,13 @@ void print256(char* label, __m256i var) {
 
 #endif
 
-void compare(__m128i* fCosts,
-             __m256i* addr,
-             __m128i fCostsA,
-             __m256i addrA,
-             __m128i fCostsB,
-             __m256i addrB) {
+void shr_n_cmp(__m128i* fCosts, __m256i* addr) {
+  // >>> 2
+  __m256i addr_shr = _mm256_permute4x64_epi64(*addr, 0x4e);
+  __m128i fCosts_shr = _mm_alignr_epi8(*fCosts, *fCosts, 8);
+
   // On récupère le fCost minimum et les adresses correspondantes
-  __m128i mask_min128 = _mm_cmpgt_epi32(fCostsA, fCostsB);
+  __m128i mask_min128 = _mm_cmpgt_epi32(*fCosts, fCosts_shr);
   __m256i mask_min256 = _mm256_cvtepi32_epi64(mask_min128);
 
   // On récupère le fCost maximum et les adresses correspondantes
@@ -46,12 +46,12 @@ void compare(__m128i* fCosts,
   __m256i mask_max256 = _mm256_cvtepi32_epi64(mask_max128);
 
   // récupération des fCosts et adresses minimum
-  __m128i min_fCosts = _mm_blendv_epi8(fCostsA, fCostsB, mask_min128);
-  __m256i min_addr = _mm256_blendv_epi8(addrA, addrB, mask_min256);
+  __m128i min_fCosts = _mm_blendv_epi8(*fCosts, fCosts_shr, mask_min128);
+  __m256i min_addr = _mm256_blendv_epi8(*addr, addr_shr, mask_min256);
 
   // récupération des fCosts et adresses maximum
-  __m128i max_fCosts = _mm_blendv_epi8(fCostsA, fCostsB, mask_max128);
-  __m256i max_addr = _mm256_blendv_epi8(addrA, addrB, mask_max256);
+  __m128i max_fCosts = _mm_blendv_epi8(*fCosts, fCosts_shr, mask_max128);
+  __m256i max_addr = _mm256_blendv_epi8(*addr, addr_shr, mask_max256);
 
   // return [min0, max0, min1, max1]
   *fCosts = _mm_unpackhi_epi32(max_fCosts, min_fCosts);
@@ -62,33 +62,42 @@ void compare(__m128i* fCosts,
   (*addr)[0] = _mm256_extract_epi64(max_addr, 0);
 }
 
+void end_cmp(__m128i* fCosts, __m256i* addr) {
+  // On récupère les deux fCosts du milieu du vecteur [3, 2, 1, 0]
+  int fCosts2 = _mm_extract_epi32(*fCosts, 2);
+  int fCosts1 = _mm_extract_epi32(*fCosts, 1);
+  if (fCosts2 > fCosts1) {
+    *addr = _mm256_permute4x64_epi64(*addr, 0xea);
+    *fCosts = (__m128i)_mm_permute_ps((__m128)*fCosts, 0xea);
+  }
+}
+
 void sse_merge_sort4x4(node_t** list, size_t size) {
   for (size_t i = 0; i < size; i += 4) {
     node_t** to_sort = list + i;
 
+    // Chargement des adresses et des fCosts correspondants
     __m256i addr = _mm256_loadu_si256((__m256i*)to_sort);
     __m128i fCosts = _mm_setr_epi32(to_sort[0]->fCost, to_sort[1]->fCost,
                                     to_sort[2]->fCost, to_sort[3]->fCost);
-
-    // >>> 2
-    __m256i addr_shr = _mm256_permute4x64_epi64(addr, 0x4e);
-    __m128i fCosts_shr = _mm_alignr_epi8(fCosts, fCosts, 8);
 
 #if DEBUG
     print128("start cost", fCosts);
     print256("start addr", addr);
 #endif
-    compare(&fCosts, &addr, fCosts, addr, fCosts_shr, addr_shr);
-
-    // >>> 2
-    addr_shr = _mm256_permute4x64_epi64(addr, 0x4e);
-    fCosts_shr = _mm_alignr_epi8(fCosts, fCosts, 8);
+    shr_n_cmp(&fCosts, &addr);
 
 #if DEBUG
-    print128("middle cost", fCosts);
-    print256("middle addr", addr);
+    print128("shr_n_cmp cost", fCosts);
+    print256("shr_n_cmp addr", addr);
 #endif
-    compare(&fCosts, &addr, fCosts, addr, fCosts_shr, addr_shr);
+    shr_n_cmp(&fCosts, &addr);
+
+#if DEBUG
+    print128("shr_n_cmp2 cost", fCosts);
+    print256("shr_n_cmp2 addr", addr);
+#endif
+    end_cmp(&fCosts, &addr);
 
     _mm256_storeu_si256((__m256i*)to_sort, addr);
 #if DEBUG
